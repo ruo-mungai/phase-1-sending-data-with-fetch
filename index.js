b02df673d007a75cde3b5a8c1d87fef0f71b9daa
Patrick <ruomungai@gmail.com>
2022-06-15 18:30:00 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+function submitData(userName, userEmail){
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: `${userName}`,
+            email: `${userEmail}`,
+        }),
+    })
+    .then(res => res.json())
+    .then(data => {
+      createObjId (data)
+    })
+    .catch(error => {
+       getError(error)
+    })
+}
+
+function createObjId(obj){
+    const userElement = document.createElement('p');
+    userElement.innerHTML = obj.id;
+        document.querySelector('body').appendChild(userElement);
+}
+
+function getError(errObj){
+    const myError = document.createElement('p');
+    myError.innerHTML = errObj.message;
+    document.querySelector('body').appendChild(myError);
+}
